9cfce0f5bf7881c2448975da63fcf4343b0c8ac7
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2024-12-13 19:29:59 -0300

    feat: add page itapema

---
+++ page-itapema.php
@@ -0,0 +1,82 @@
+<?php
+// Template Name: Itapema
+?>
+
+<?php get_header(); ?>
+
+<main id="pg-location">
+
+    <section class="intro">
+        <img class="img" src="<?php echo get_stylesheet_directory_uri() . "/img/page-itapema.webp"; ?>"
+            alt="Imagem Sobre nós">
+        <div class="content container">
+            <p>Connheça</p>
+            <h1>Itapema</h1>
+        </div>
+        <a href="#infos" class="link">desça</a>
+    </section>
+
+    <section class="about container">
+        <h2 class="title">Historia e cultura</h2>
+        <img class="img" src="<?php echo get_stylesheet_directory_uri() . "/img/itapema-2.webp"; ?>"
+            alt="Foto de Itapema">
+        <div class="texts">
+            <div>
+                <p>Itapema une <strong>tradição</strong> e <strong>modernidade</strong> em <strong>perfeita
+                        harmonia</strong>. De suas raízes como vila de pescadores, tornou-se um dos destinos mais
+                    desejados de Santa Catarina. A cultura local é <strong>profundamente</strong> ligada ao mar, com
+                    <strong>festas típicas</strong> como a Festa do Divino e a Festa de São Pedro, que celebram
+                    artesanato, música e culinária fresquíssima.
+                </p>
+                <p>
+                    Preservando suas origens, Itapema combina <strong>desenvolvimento sustentável</strong> com história
+                    e identidade,
+                    fazendo de cada rua uma <strong>história viva</strong>, onde o passado encontra o presente.</p>
+            </div>
+            <div>
+                <p>Banhada pelo sol e cercada de natureza, Itapema é um <strong>paraíso singular</strong> de belezas
+                    naturais e atrativos
+                    turísticos. Como a <strong>terceira</strong> cidade mais visitada de Santa Catarina, encanta com
+                    praias de <strong>águas
+                        cristalinas</strong>, mirantes e voos panorâmicos – sendo a capital catarinense de
+                    <strong>ultraleves</strong>.
+                    <strong>Trilhas</strong> e o
+                    <strong>calçadão</strong> da Meia Praia convidam a explorar cada detalhe desse refúgio.
+                </p>
+                <p>Itapema exibe toda a <strong>riqueza</strong> do <strong>litoral brasileiro</strong>, proporcionando
+                    fins de tarde <strong>inesquecíveis</strong> para
+                    quem <strong>visita</strong> ou escolhe <strong>viver</strong> aqui.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="infos">
+        <div class="container">
+            <div class="img">
+                <h2>Lazer e</br> Qualidade de</br> Vida</h2>
+                <img class="img" src="<?php echo get_stylesheet_directory_uri() . "/img/itapema-3.webp"; ?>"
+                    alt="Foto de Itapema">
+            </div>
+            <div class="content">
+                <div>
+                    <p>Itapema oferece qualidade de vida com <strong>praias de águas claras</strong>, calçadão para
+                        <strong>caminhadas</strong> e <strong>trilhas</strong> com
+                        <strong>vistas deslumbrantes</strong>. Além do <strong>lazer ao ar livre</strong>, a cidade
+                        dispõe
+                        de excelentes <strong>restaurantes</strong>, <strong>bares</strong> e
+                        <strong>centros de compras</strong>, proporcionando momentos de <strong>relaxamento</strong> e
+                        <strong>diversão</strong>.
+                    </p>
+                    <p>Viver em Itapema é adotar um estilo de vida <strong>saudável e ativo</strong>, onde o mar e a
+                        natureza estão sempre
+                        por perto.</p>
+                </div>
+                <img class="img" src="<?php echo get_stylesheet_directory_uri() . "/img/itapema-4.webp"; ?>"
+                    alt="Foto de Itapema">
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php get_footer(); ?>
